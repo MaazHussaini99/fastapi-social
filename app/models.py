45c328ff1b3db257469b4e7efced97431d99076f
Maaz Hussaini <maazhussaini@outlook.com>
2023-06-25 21:01:07 -0400

    added votes table to sqlalchemy

--- app/models.py
+++ app/models.py
@@ -25,3 +25,12 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("post.id", ondelete="CASCADE"), primary_key=True)
+
+
+# paused at 9:33:08 on 6/25/23
